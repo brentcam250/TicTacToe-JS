3972a2be2e53951f2c90fdc4f6ecd6626fa11a76
Brent Cameron <bjcameron@gmail.com>
2021-12-10 07:59:25 -0700

    linked button to startgame funct in  js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 
-const playerFactory = (name, letter) => {
+// const playerFactory = (name, letter) => {
 
-}
+// }
 
 const gameboard = (() => {
-  this.init = () =>{
-    let gameboard = [];
-    let turn_counter = 0;
-  }
+  // let gameboard = [];
+  // let gridElement = document.getElementById('grid');
+  let testElement = 'testing';
+
+  const output = () => console.log(testElement);
+  // this.init = () =>{
+  //   // let turn_counter = 0;
+  //   for (i = 0; i < 9 i ++){
+  //     let cell = document.createElement('div');
+  //     cell.id = `grid-${i}`;
+  //     gameboard.push(cell);
+  //     gridElement.appendChild(cell);
+  //   }
+  // }
+  return {output}
 
 })
+
+function startGame(){
+  console.log("hello I hear you ");
+  const board = gameboard();
+  board.output;
+}
