3c2fd707f20acf5007518b93b57339e799cd992a
Brent Cameron <bjcameron@gmail.com>
2021-12-21 08:51:55 -0700

    hide board before the start game, display whos turn it is

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const gameboard = (() => {
   // let gameboard = [];
   // let gridElement = document.getElementById('grid');
   let testElement = 'testing';
+  let boxes = document.querySelectorAll(".grid-item");
+  let g1 = document.getElementById('grid-1');
+  // g1.style.display 
+  // boxes.style.display = 'block';
+  // console.log(boxes.length());
+  boxes.forEach((box) => {
+    box.style.display = 'flex';
+  });
 
   const output = () => console.log(testElement);
   // this.init = () =>{
@@ -21,6 +29,13 @@ const gameboard = (() => {
   //     gridElement.appendChild(cell);
   //   }
   // }
+
+  // let boxes = document.querySelectorAll(".grid-item");
+  boxes.forEach((div)=> {
+    div.addEventListener('click', function () {
+      console.log('HI thanks for clicking me');
+    })
+  })
   return {output}
 
 })
@@ -30,7 +45,8 @@ function startGame(){
   let players = setPlayers();
   player1 = players[0];
   player2 = players[1];
-  console.log(player1.getLetter);
+  console.log(player1.getName);
+  setTurn(player1);
 
   board.output;
 }
@@ -41,17 +57,24 @@ function setPlayers(){
   let player2_name = prompt("please enter your name", "Player2");
   let player2 = Player(player2_name, 'O')
   let p1_id = document.getElementById('player1');
-  p1_id.innerHTML = player1_name;
+  p1_id.innerHTML = `${player1_name} - your letter: "X"`;
   let p2_id = document.getElementById('player2');
-  p2_id.innerHTML = player2_name;
+  p2_id.innerHTML = `${player2_name} - your letter: "O"`;
   vs = document.getElementById("vs");
   vs.innerHTML = "VS"
   return [player1, player2]
 
 }
 
-const grid1 = document.getElementById("grid-1");
-grid1.addEventListener("click", function(event){
-  console.log('click grid 1');
-})
+function setTurn(player){
+  let turn_display = document.getElementById('turn-counter');
+  let name = player.getName
+  turn_display.innerHTML = `${name}'s Turn`;
+
+}
+
+// const grid1 = document.getElementById("grid-1");
+// grid1.addEventListener("click", function(event){
+//   console.log('click grid 1');
+// })
 
