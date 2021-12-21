3095a278cb385c5bfc9ab6f9fd1b99c0cee0a24d
Brent Cameron <bjcameron@gmail.com>
2021-12-21 07:13:11 -0700

    added styles for header, added prompt to collect players names, create new players

--- script.js
+++ script.js
@@ -28,16 +28,34 @@ const gameboard = (() => {
 function startGame(){
   console.log("hello I hear you ");
   const board = gameboard();
-  const testPlayer = Player("BC", "X");
-  console.log(testPlayer.getLetter);
-  console.log(testPlayer.getName);
+  // window.prompt('Please enter your name', 'Player1');
+  setPlayers();
+
+  // const testPlayer = Player("BC", "X");
+  // console.log(testPlayer.getLetter);
+  // console.log(testPlayer.getName);
 
   // const player1 = PlayerFactory("Brent", "X");
   // console.log(player1.getLetter);
   board.output;
 }
 
+function setPlayers(){
+  let player1_name = prompt("please enter your name", "Player1");
+  let player1 = Player(player1_name, 'X')
+  let player2_name = prompt("please enter your name", "Player2");
+  let player2 = Player(player2_name, 'O')
+  let p1_id = document.getElementById('player1');
+  p1_id.innerHTML = player1_name;
+  let p2_id = document.getElementById('player2');
+  p2_id.innerHTML = player2_name;
+  vs = document.getElementById("vs");
+  vs.innerHTML = "VS"
+
+}
+
 const grid1 = document.getElementById("grid-1");
 grid1.addEventListener("click", function(event){
   console.log('click grid 1');
 })
+
