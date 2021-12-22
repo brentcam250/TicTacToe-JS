3b1127b65ca91af024ed94a944cc4ca0f9a5f1b4
Brent Cameron <bjcameron@gmail.com>
2021-12-22 10:06:14 -0700

    checks if winner, and blocks any more moves after a winner

--- script.js
+++ script.js
@@ -11,19 +11,17 @@ const gameboard = (() => {
 
   let boxes = document.querySelectorAll(".grid-item");
   // let turn_counter = 0;
-  let board_array = [];
+  let board_array = [0,0,0,0,0,0,0,0];
 
+  // display the grid now
   boxes.forEach((box) => {
     box.style.display = 'flex';
+    box.innerHTML = '';
+    let button = document.getElementById('game-button');
+    button.innerHTML = 'Restart Game'
   });
 
 
-  // boxes.forEach((div)=> {
-  //   div.addEventListener('click', function () {
-  //     click_cell(div);
-
-  //   })
-  // })
   const get_element = (id) => {
     if (0 <= id && id < 9){
       return board_array[id]
@@ -31,41 +29,54 @@ const gameboard = (() => {
       console.log(`trying to check an ID: ${id} outside of the boards array`);
     }
   }
-  // const play_cell = (cell, player) => {
-  //   console.log(`playing cell ${cell}, ${player}`)
-  // }
 
-  // const click_cell = (div, letter) => {
-  //     //get the array index
-  //     let index = parseInt(div.id.charAt(div.id.length-1)-1);
-  //     if (get_element(index)) {
-  //       console.log("this cell is played");
-  //       // return 0
-  //       // not played
-  //     }else{
-  //       console.log("this cell is free");
+  const set_element = (div, letter, index) => {
+    board_array[index] = letter;
+    div.innerHTML = letter;
 
-  //       div.innerHTML = letter;
+  }
 
+  // winning combos: 
+  // horz:012, 345, 678
+  // vert:036, 147, 258
+  // angle:048, 642
+  const check_winner = () => {
+    // console.log([get_element(0), get_element(1), get_element(2)].join(''))
+    let winning_combos = [ 
+      [get_element(0), get_element(1), get_element(2)].join(''), 
+      [get_element(3), get_element(4), get_element(5)].join(''), 
+      [get_element(6), get_element(7), get_element(8)].join(''),
+      [get_element(0), get_element(3), get_element(6)].join(''), 
+      [get_element(1), get_element(4), get_element(7)].join(''), 
+      [get_element(2), get_element(5), get_element(8)].join(''),
+      [get_element(0), get_element(4), get_element(8)].join(''), 
+      [get_element(6), get_element(4), get_element(2)].join('')
+    ];
 
-  //       // return 1
-  //       //played
-  //       // let current_player = get_player();
-  //       // console.log(`currently ${current_player}'s turn `);
-  //       // return index;
-  //       // let the gameplay object know who was clicked 
+    // console.log("just letting you know im checkin");
+    //check all combos for a winner
+    let output = 0;
+    winning_combos.forEach(combo => {
+      console.log(`checking ${combo}`);
+      if (combo.localeCompare('XXX') == 0){
+        // window.alert(`WINNER! ${combo}`)
+        console.log("Winner player1 ")
 
-  //     }
+        // return 1;
+        output = 1;
+      }else if (combo.localeCompare('OOO') == 0){
+        // window.alert(`WINNER! ${combo}`)
+        console.log("Winner player2 ")
 
-  // }
-  // const get_player = () => {
-  //   return playGame().get_player();
-  // }
-  // const play_cell = (index, letter) => {
+        output = 2;
+      }
+    })
+    return output;
+    // console.log(winning_combos);
 
-  // }
+  }
 
-  return {get_element} 
+  return {get_element, check_winner, set_element} 
 
 });
 
@@ -82,26 +93,36 @@ const playGame = (() => {
   boxes.forEach((div)=> {
     div.addEventListener('click', function () {
       click_cell(div, get_player().get_letter());
-      // if played_bool {
-      //   console.log('played');
-      // }
 
     })
   })
 
 
   const click_cell = (div, letter) => {
-    let index = parseInt(div.id.charAt(div.id.length-1)-1);
-    if (board.get_element(index)) {
-      console.log("this cell is played");
-      // return 0
-      // not played
-    }else{
-      console.log("this cell is free");
-
-      div.innerHTML = letter;
-      increment_turn();
+    if (board.check_winner()){
+      return 
+    }else {
+      let index = parseInt(div.id.charAt(div.id.length-1)-1);
+      if (board.get_element(index)) {
+        console.log("this cell is played");
+        // return 0
+        // not played
+      }else{
+        console.log("this cell is free");
+  
+        board.set_element(div, letter, index)
+        let winner_bool = board.check_winner();
+        console.log(`winner bool ${winner_bool}`)
+        if (winner_bool) {
+          // if winner_bool == 1 {
+            // window.alert(`WINNER! ${letter}`)
+            console.log('winna')
+          // }
+        }
+        increment_turn();
+      }
     }
+
   }
 
 
@@ -127,6 +148,9 @@ const playGame = (() => {
   }
 
   const increment_turn = () => {
+    if (turn_count > 8 ){
+      window.alert("game ova ");
+    }
     turn_count++;
     current_player = turn_count %2;
     set_turn(get_player(), get_turn_count());
